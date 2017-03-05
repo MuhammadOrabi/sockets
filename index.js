02dd35e55da29c7e970e20cd1a69b05b8041d4c0
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2017-03-05 11:27:51 +0200

    Add Sockets

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 app.use(express.static(__dirname + '/public'));
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+	socket.on('message', function (msg) {
+		console.log('Message: ' + msg.text);
+		msg.date = moment().valueOf();
+		io.emit('message', msg);
+	});
+	socket.emit('message', {
+		text: 'Welcome to the chat app!',
+		date: moment().valueOf()
+	});
 });
 http.listen(PORT, function () {
 	console.log('Server Start!');
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,27 @@
 var socket = io();
+
 socket.on('connect', function () {
 	console.log('connected to socket.io server!');
+});
+
+var vm = new Vue({
+  	el: '#app',
+  	data: {
+  		msg: '',
+  		msgs: []
+  	},
+  	mounted: function () {
+  		socket.on('message', function (msg) {
+  			var date = moment.utc(msg.date);
+			this.msgs.push({msg: msg.text, date: date});
+		}.bind(this));
+  	},
+  	methods: {
+		sendMessage: function () {
+			socket.emit('message', {
+				text: this.msg
+			});
+			this.msg = '';
+		}
+  	}
 })
